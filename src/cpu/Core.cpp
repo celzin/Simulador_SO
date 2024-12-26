8a674f0c41c2c031b9ca87e72d5871b89c0da903
celso_wsl_note <celsovinicius4@gmail.com>
2024-12-26 15:29:53 -0300

    refactor[Core]: ajustando a execução do processo para interagir com os estados de bloqueio

--- src/cpu/Core.cpp
+++ src/cpu/Core.cpp
@@ -1,56 +1,73 @@
 #include "../includes/Core.hpp"
 #include <mutex>
+#include <thread>
+#include <iostream>
 
 std::mutex coutMutex;
 
-Core::Core(RAM& ram, Disco& disco, ProcessManager& pm)
+Core::Core(RAM &ram, Disco &disco, ProcessManager &pm)
     : ram(ram), disco(disco), processManager(pm), PC(0), Clock(0), isRunning(false) {}
 
-void Core::start() {
+void Core::start()
+{
     isRunning = true;
     coreThread = std::thread(&Core::run, this);
 }
 
-void Core::stop() {
+void Core::stop()
+{
     isRunning = false;
-    if (coreThread.joinable()) {
+    if (coreThread.joinable())
+    {
         coreThread.join();
     }
 }
 
-void Core::run() {
-    while (isRunning) {
-        if (processManager.temProcessosProntos()) {
+void Core::run()
+{
+    while (isRunning)
+    {
+        if (processManager.temProcessosProntos())
+        {
             ProcessControlBlock pcb = processManager.obterProximoProcesso();
             pcb.state = EXECUTANDO;
-            
+
             {
                 std::lock_guard<std::mutex> lock(coutMutex);
-                std::cout << "[Core " << this_thread::get_id() << "]" << " Executando processo ID: " << pcb.processID << std::endl;
+                std::cout << "[Core " << this_thread::get_id() << "]"
+                          << " Executando processo ID: " << pcb.processID << std::endl;
             }
-        
+
             executarProcesso(pcb);
 
-            if (pcb.quantum > 0) {
+            if (pcb.state == PRONTO && pcb.quantum > 0)
+            {
                 pcb.saveState(PC, regs);
-                pcb.state = PRONTO;
                 processManager.adicionarProcesso(pcb);
-            } else {
+            }
+            else if (pcb.state == BLOQUEADO)
+            {
+                processManager.bloquearProcesso(pcb, pcb.resource);
+            }
+            else
+            {
                 {
                     std::lock_guard<std::mutex> lock(coutMutex);
                     std::cout << "Processo ID " << pcb.processID << " concluído." << std::endl;
                 }
             }
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Simula tempo de clock
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
 
-void Core::executarProcesso(ProcessControlBlock& pcb) {
+void Core::executarProcesso(ProcessControlBlock &pcb)
+{
     PC = pcb.PC;
     regs = pcb.regs;
 
-    while (pcb.quantum > 0 && isRunning) {
+    while (pcb.quantum > 0 && isRunning)
+    {
         // uc.executarInstrucao(regs, ram, PC, disco, Clock);
         pcb.quantum--;
         {
