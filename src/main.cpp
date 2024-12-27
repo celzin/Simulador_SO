2b298ab539a2d2c79f7e72b587ca886c60774e30
celso_wsl_note <celsovinicius4@gmail.com>
2024-12-27 20:12:28 -0300

    test[main]: testando integração da PCB com o Core

--- src/main.cpp
+++ src/main.cpp
@@ -17,31 +17,35 @@
 using namespace std;
 
 int main() {
-    // Criação do gerenciador de processos
+    RAM ram;
+    Disco disco;
+
+    // Criando um único núcleo
+    Core core(ram, disco);
+
+    // Gerenciador de processos
     GerenciadorProcessos gerenciador;
 
-    // Criação de instruções fictícias
+    // Criando instruções fictícias
     std::vector<Instruction> instr1 = {Instruction(ADD, 0, 1, 2), Instruction(SUB, 1, 0, 3)};
     std::vector<Instruction> instr2 = {Instruction(MULT, 2, 3, 4), Instruction(DIV, 4, 3, 2)};
 
-    // Criação de processos
+    // Criando processos
     PCB processo1(1, instr1, 5); // ID: 1, Quantum: 5
     PCB processo2(2, instr2, 5); // ID: 2, Quantum: 5
 
-    // Adicionar processos ao gerenciador
+    // Adicionando processos ao gerenciador
     gerenciador.adicionarProcesso(&processo1);
     gerenciador.adicionarProcesso(&processo2);
 
-    // Mostrar estados iniciais
-    gerenciador.mostrarEstados();
+    // Executando processos manualmente no núcleo
+    PCB* processoAtual = gerenciador.obterProximoProcesso();
+    core.setProcesso(processoAtual);
+    core.executarProcesso();
 
-    // Bloquear um processo e mostrar estados
-    gerenciador.bloquearProcesso(&processo1);
-    gerenciador.mostrarEstados();
-
-    // Desbloquear processos e mostrar estados
-    gerenciador.desbloquearProcessos();
-    gerenciador.mostrarEstados();
+    processoAtual = gerenciador.obterProximoProcesso();
+    core.setProcesso(processoAtual);
+    core.executarProcesso();
 
     return 0;
 }
