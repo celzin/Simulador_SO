7078c0b58e3aebeb8d06eb21fd8db412332b960f
celso_wsl_note <celsovinicius4@gmail.com>
2024-12-28 11:31:28 -0300

    test[main]: adicionando testes para novas funcoes de paginacao

--- src/main.cpp
+++ src/main.cpp
@@ -12,24 +12,36 @@ int main() {
     RAM ram;
 
     // Processo 1
-    int pagina1 = ram.alocarPagina(1); // Página lógica 0 do Processo 1
+    int pagina1_proc1 = ram.alocarPagina(1);
     ram.currentProcessId = 1;
-    ram.write(pagina1 * RAM::tamanhoPagina, 42); // Escreve na página lógica 0
+    ram.write(pagina1_proc1 * RAM::tamanhoPagina, 100);
+
+    int pagina2_proc1 = ram.alocarPagina(1);
+    ram.write(pagina2_proc1 * RAM::tamanhoPagina + 2, 200);
 
     // Processo 2
-    int pagina2 = ram.alocarPagina(2); // Página lógica 0 do Processo 2
+    int pagina1_proc2 = ram.alocarPagina(2);
     ram.currentProcessId = 2;
-    ram.write(pagina2 * RAM::tamanhoPagina + 1, 99); // Escreve na página lógica 0 com offset 1
+    ram.write(pagina1_proc2 * RAM::tamanhoPagina + 1, 300);
 
     // Leitura
     ram.currentProcessId = 1;
-    int valor1 = ram.read(pagina1 * RAM::tamanhoPagina);
+    std::cout << "Valor Processo 1, Página 1: " 
+              << ram.read(pagina1_proc1 * RAM::tamanhoPagina) << std::endl;
+
+    std::cout << "Valor Processo 1, Página 2: " 
+              << ram.read(pagina2_proc1 * RAM::tamanhoPagina + 2) << std::endl;
 
     ram.currentProcessId = 2;
-    int valor2 = ram.read(pagina2 * RAM::tamanhoPagina + 1);
+    std::cout << "Valor Processo 2, Página 1: " 
+              << ram.read(pagina1_proc2 * RAM::tamanhoPagina + 1) << std::endl;
 
-    std::cout << "Valor 1 lido: " << valor1 << std::endl; // Deve ser 42
-    std::cout << "Valor 2 lido: " << valor2 << std::endl; // Deve ser 99
+    // Liberar páginas
+    ram.liberarPagina(1);
+    ram.liberarPagina(2);
+
+    ram.display();
 
     return 0;
 }
+
