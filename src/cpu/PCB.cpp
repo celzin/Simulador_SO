3151b3b754cf755a052a91254731684d5d52d2d8
celso_wsl_note <celsovinicius4@gmail.com>
2024-12-28 10:44:24 -0300

    feat/refactor[PCB]: adicionando tabela de paginas ao pcb

--- src/cpu/PCB.cpp
+++ src/cpu/PCB.cpp
@@ -1,7 +1,12 @@
 #include "../includes/PCB.hpp"
 
+// PCB::PCB(int id, const std::vector<Instruction>& instrs, int quantumInicial)
+//     : pid(id), estado(PRONTO), PC(0), quantum(quantumInicial), registradores(12, 0), instrucoes(instrs) {}
+
 PCB::PCB(int id, const std::vector<Instruction>& instrs, int quantumInicial)
-    : pid(id), estado(PRONTO), PC(0), quantum(quantumInicial), registradores(12, 0), instrucoes(instrs) {}
+    : pid(id), estado(PRONTO), PC(0), quantum(quantumInicial), instrucoes(instrs) {
+    registradores.resize(12, 0); // Inicializar 12 registradores com valor 0
+}
 
 void PCB::displayPCB() const {
     std::cout << "=== PCB do Processo " << pid << " ===" << std::endl;
--- src/includes/PCB.hpp
+++ src/includes/PCB.hpp
@@ -1,6 +1,7 @@
 #ifndef PCB_HPP
 #define PCB_HPP
 
+#include <map>
 #include <vector>
 #include <string>
 #include <iostream>
@@ -15,12 +16,13 @@ enum ProcessState {
 
 class PCB {
 public:
-    int pid;                         // Identificador único do processo
-    ProcessState estado;             // Estado atual do processo
-    int PC;                          // Contador de programa
-    int quantum;                     // Quantum restante
-    std::vector<int> registradores;  // Registradores do processo
+    int pid;                             // Identificador único do processo
+    ProcessState estado;                 // Estado atual do processo
+    int PC;                              // Contador de programa
+    int quantum;                         // Quantum restante
+    std::vector<int> registradores;      // Registradores do processo
     std::vector<Instruction> instrucoes; // Instruções carregadas na memória
+    std::map<int, int> tabelaPaginas;    // Mapeamento de páginas virtuais para físicas
 
     PCB(int id, const std::vector<Instruction>& instrs, int quantumInicial);
     void displayPCB() const;
